24dea6fb39c38f2157f9a66a3bf0376c11d76ee5
Jhois <>
2017-11-28 14:54:42 -0500

    primer commmit

---
+++ CompuManual/src/compumanual/CompuManual.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compumanual;
+
+/**
+ *
+ * @author Inspiron
+ */
+public class CompuManual {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("1.-ingresar gitup");
+    }
+    
+}
